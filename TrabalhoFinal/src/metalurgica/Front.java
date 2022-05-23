eddc6139cc38bac68e13246087d8ac9244edce7e
TheS1lentKiller <86420081+TheS1lentKiller@users.noreply.github.com>
2022-05-23 02:33:09 -0300

    Delete TrabalhoFinal directory

--- TrabalhoFinal/src/metalurgica/Front.java
+++
@@ -1,9 +0,0 @@
-package metalurgica;
-
-public class Front {
-	public static void main(String[] args) {
-		Cliente c = new Cliente();
-		
-		c.mostrarDados();
-	}
-}
--- TrabalhoFinal/src/metalurgica/Gerente.java
+++
@@ -1,5 +0,0 @@
-package metalurgica;
-
-public class Gerente {
-
-}
